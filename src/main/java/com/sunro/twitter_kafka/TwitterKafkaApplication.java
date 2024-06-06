27627ed99ebc7f80f03e2eb8dc7f619abd6972fe
Sunro Lee <tjsfhdhkd@naver.com>
2024-06-07 00:25:19 +0900

    Initial Commit

---
+++ src/main/java/com/sunro/twitter_kafka/TwitterKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sunro.twitter_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunro/twitter_kafka/TwitterKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunro.twitter_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
